c3778102d01661d87666aa9e6b9ccb46c0150781
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-01-15 16:55:23 -0500

    just added img

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import "./styles.css";
 import { home } from "./home.js"
 import { menu } from "./menu.js"
 
+let homeButton = document.querySelector("#home")
+let menuButton = document.querySelector("#menu")
+let aboutButton = document.querySelector("#about")
+let contactButton = document.querySelector("#contact")
 
-menu();
+homeButton.addEventListener("click", function () {
+  home();
+});
+
+menuButton.addEventListener("click", function () {
+  menu();
+});
+
+aboutButton.addEventListener("click", function () {
+
+});
+
+contactButton.addEventListener("click", function () {
+
+});
